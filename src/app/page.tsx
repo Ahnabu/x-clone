29c66ac0594c8d7c0029ceec4f760615f2d8d967
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-01-30 19:19:32 +0600

    post info created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import Feed from "@/components/Feed"
+import Share from "@/components/Share"
+import Link from "next/link"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <div className="flex justify-around text-textGray font-bold border-b-[1px] border-borderGray">
+        <Link href="/" className="pb-3 flex items-center border-b-4 border-iconBlue "> For you</Link>
+        <Link href="/" className="pb-3 flex items-center "> Following</Link>
+      </div>
+      <Feed />
+      <Share />
+    </div>
   )
 }
 
